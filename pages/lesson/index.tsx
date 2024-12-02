206a0444c71b80c3d54dd97b338039591eb3b393
t-custom <wvts2001@gmail.com>
2024-12-02 11:32:24 +0900

    fix レッスンページは一旦閉鎖

--- pages/lesson/index.tsx
+++ pages/lesson/index.tsx
@@ -1,16 +1,16 @@
 // import Header from '@/src/components/Header';
 // import Footer from '@/src/components/Footer';
-import BackgroundVideo from '@/src/components/BackgroundVideo';
+// import BackgroundVideo from '@/src/components/BackgroundVideo';
 
 const LessonPage: React.FC = () => {
   return (
     <>
     {/* <Header /> */}
 
-    <BackgroundVideo videoUrl="/movies/movie1.mp4">
+    {/* <BackgroundVideo videoUrl="/movies/movie1.mp4">
       <h1>Welcome to the Lesson Page</h1>
       <p>Enjoy the immersive experience!</p>
-    </BackgroundVideo>
+    </BackgroundVideo> */}
 
     {/* <Footer /> */}
     </>
